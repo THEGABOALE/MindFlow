687423d7c839d199cc6d2bfbc5243e6ed297413c
Garu006 <garojas@uamv.edu.ni>
2026-07-09 16:10:33 -0600

    core(db): configurar conexión PostgreSQL

---
+++ backend/src/database/connection.js
@@ -0,0 +1,12 @@
+const { Pool } = require("pg");
+const env = require("../config/env");
+
+const pool = new Pool({
+  host: env.db.host,
+  port: env.db.port,
+  database: env.db.name,
+  user: env.db.user,
+  password: env.db.password
+});
+
+module.exports = pool;
